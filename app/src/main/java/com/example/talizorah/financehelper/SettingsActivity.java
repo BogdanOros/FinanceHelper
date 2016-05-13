94e81a1685b32804d193761b36953213aa6d6202
TaliZorah <orossbogdan@gmail.com>
2016-05-13 20:11:13 +0300

    fixed bug

--- app/src/main/java/com/example/talizorah/financehelper/SettingsActivity.java
+++ app/src/main/java/com/example/talizorah/financehelper/SettingsActivity.java
@@ -18,7 +18,7 @@ import android.preference.PreferenceManager;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsActivity extends AppCompatPreferenceActivity
+public class SettingsActivity extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener {
 
     @Override
